536dafeba462164708428acb5ad232b6100e4990
Mingyu-Potato <mingyu6952@gmail.com>
2021-09-28 01:16:49 +0900

    etc.

--- file_remove, save.py
+++ file_remove, save.py
@@ -13,15 +13,31 @@ import os
 #         # os.remove(file_path)
 
 
-# 파일 이름에 따른 파일 저장
-file_list = os.listdir('./SFR-03(Abnormal)/Sagging_Image')
+# # 파일 이름에 따른 파일 저장
+# file_list = os.listdir('./SFR-03(Abnormal)/Sagging_Image')
+
+# for f in file_list:
+#     filename, file_extension = os.path.splitext(f)
+#     # print("파일명: ", f, "     확장자: ", file_extension)
+#     if filename[-6:] == 'NORMAL':
+#         file_path = os.path.join('./SFR-03(abnormal)/Sagging_Image', f)
+#         new_file_path = os.path.join('./SFR-03(abnormal)/NORMAL', f)
+#         print("파일 경로 + 파일명 = ", file_path)
+        
+#         shutil.copy(file_path, new_file_path)
+
+
+# 파일 이름 바꾸기
+file_list = os.listdir('./image/SFR-03(Abnormal)/포토샵작업_처짐_Aug')
 
 for f in file_list:
     filename, file_extension = os.path.splitext(f)
+    file_num = filename[5:]
     # print("파일명: ", f, "     확장자: ", file_extension)
-    if filename[-6:] == 'NORMAL':
-        file_path = os.path.join('./SFR-03(abnormal)/Sagging_Image', f)
-        new_file_path = os.path.join('./SFR-03(abnormal)/NORMAL', f)
-        print("파일 경로 + 파일명 = ", file_path)
+    if filename[:4] == 'HARD':
+        new_file_name = "HIGH_" + file_num + ".jpg"
+        file_old_name = os.path.join('./image/SFR-03(abnormal)/포토샵작업_처짐_Aug', f)
+        file_new_name = os.path.join('./image/SFR-03(abnormal)/포토샵작업_처짐_Aug', new_file_name)
+        print("파일 경로 + 파일명 = ", file_new_name)
         
-        shutil.copy(file_path, new_file_path)
+        os.rename(file_old_name, file_new_name)
